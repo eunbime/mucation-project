58365ca3327f93977e2155bebff66a906b4d2095
Kim MyungSub <audtjqxx@naver.com>
2023-12-07 11:35:13 +0900

    write페이지 styles 파일 분리 / 동영상 정보 state 초기화 값 변경

--- src/pages/write/WritePageContext.jsx
+++ src/pages/write/WritePageContext.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+import { StContextSection } from './write.styles';
 
 const WritePageContext = ({ contextValue, setContextValue }) => {
   return (
@@ -8,23 +8,5 @@ const WritePageContext = ({ contextValue, setContextValue }) => {
     </StContextSection>
   );
 };
-const StContextSection = styled.section`
-  width: 100%;
-  height: 10rem;
-  font-size: 1.25rem;
-  margin: 0 0 2rem 0;
 
-  & textarea {
-    font-family: inherit;
-    font-size: inherit;
-    padding: 1.25rem;
-    width: 100%;
-    height: 100%;
-    resize: none;
-    outline: none;
-    border: 1px solid #fff;
-    background-color: transparent;
-    color: #fff;
-  }
-`;
 export default WritePageContext;
