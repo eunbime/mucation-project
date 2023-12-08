9317a3caf709f4806e55c2e91492ef239cfcfa88
jihyun-j <jihyun9142@gmail.com>
2023-12-08 15:07:27 +0900

    사용자 프로필 수정 및 관심사 추가 기능 진행 중

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { QueryClient, QueryClientProvider } from 'react-query';
-// import { Provider } from 'react-redux';
-// import store from 'redux/config/configStore';
+import { Provider } from 'react-redux';
+import store from './redux/config/configStore';
 
 const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <Provider store={store}>
   <QueryClientProvider client={queryClient}>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </QueryClientProvider>
-  // </Provider>
 );
