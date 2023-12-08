96a5f9fd3d7d4b2e4c2bcd50064382300aa11c74
Kim MyungSub <audtjqxx@naver.com>
2023-12-08 20:21:24 +0900

    detail 페이지 접근 변경, 페이지별 로그인 여부 확인 로직 추가, 쇼셜로그인 아이콘 변경

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -79,17 +79,19 @@ export const useAuth = () => {
     auth.onAuthStateChanged((user) => {
       if (!user) {
         console.log('토큰이 없습니다.');
+        dispatch(setSuccessLogout());
         return;
       }
       if (currentDate > user?.stsTokenManager.expirationTime) {
-        // console.log('현재시간', currentDate);
-        // console.log('만료시간', user?.stsTokenManager.expirationTime);
-        // console.log('유효한 토큰이니?>', currentDate <= user?.stsTokenManager.expirationTime);
+        //console.log('현재시간', currentDate);
+        //console.log('만료시간', user?.stsTokenManager.expirationTime);
+        //console.log('유효한 토큰이니?>', currentDate <= user?.stsTokenManager.expirationTime);
         console.log('유효하지 않은 토큰입니다.');
+        dispatch(setSuccessLogout());
       } else {
-        // console.log('현재시간', currentDate);
-        // console.log('만료시간', user?.stsTokenManager.expirationTime);
-        // console.log('유효한 토큰이니?>', currentDate <= user?.stsTokenManager.expirationTime);
+        //console.log('현재시간', currentDate);
+        //console.log('만료시간', user?.stsTokenManager.expirationTime);
+        //console.log('유효한 토큰이니?>', currentDate <= user?.stsTokenManager.expirationTime);
         console.log('유효한 토큰입니다.');
         dispatch(setSuccessLogin());
       }
--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import UserCard from './UserCard';
 import UserPostCard from './UserPostCard';
 import EditProfileModal from './EditProfileModal';
 import { useSelector } from 'react-redux';
+import { useAuth } from 'hooks/useAuth';
 
 const Profile = () => {
   const { isEditingUserProfile } = useSelector((state) => state.auth);
+
+  const { checkAuth } = useAuth();
+  useEffect(() => {
+    checkAuth();
+  }, []);
 
   return (
     <>
