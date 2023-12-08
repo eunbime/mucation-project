3309d1612282e08ffe1c60c9c0956b468bbf3e3d
Kim MyungSub <audtjqxx@naver.com>
2023-12-08 17:35:56 +0900

    폴더이름 변경

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import PostList from 'components/post-list/PostList';
+import PostList from 'components/Post-list/PostList';
 import Location from 'components/location/Location';
 import { StHomeContainer } from './Home.styles';
 
