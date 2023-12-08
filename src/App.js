c7b900dcc9f2d3047318a5ce5cab693b47816da4
ByeongTaek <respect21047436@gmail.com>
2023-12-08 10:54:06 +0900

    app 리액트 쿼리 useQueryClient 오류

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import GlobalStyles from 'styles/GlobalStyle';
 import Router from 'shared/Router';
+// import { QueryClientProvider, useQueryClient } from 'react-query';
+
+// const queryClient = useQueryClient();
 
 function App() {
   return (
     <>
+   
       <GlobalStyles />
+      {/* <QueryClientProvider client={queryClient}> */}
       <Router />
+      {/* </QueryClientProvider> */}
     </>
   );
 }
