99001f49f37260f1de09f9bfed50f315093f7f0b
ByeongTaek <respect21047436@gmail.com>
2023-12-09 16:39:44 +0900

    풀 당길것

--- src/App.js
+++ src/App.js
@@ -37,9 +37,9 @@ function App() {
      
      
       <GlobalStyles />
-      <QueryClientProvider client={queryClient}>
+      {/* <QueryClientProvider client={queryClient}> */}
       <Router />
-      </QueryClientProvider>
+      {/* </QueryClientProvider> */}
     </>
   );
 }
