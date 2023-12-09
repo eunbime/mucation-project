eeb7a8e2a6c1adc5fe646c532246fba1e8e27f31
Kim MyungSub <audtjqxx@naver.com>
2023-12-09 17:07:27 +0900

    메인카드 유저정보 추가 / 게시물 삭제 후 재 생성 해야함

--- src/hooks/useAlert.js
+++ src/hooks/useAlert.js
@@ -6,12 +6,17 @@ const useAlert = () => {
   const alertInfo = useSelector((state) => state.alertSlice);
   const dispatch = useDispatch();
 
-  // confirm
+  // confirm 사용시
   const confirm = ({ title, message }) => {
     dispatch(confirmOpen({ title, message }));
     return new Promise((resolve) => {
       dispatch(setResolveFunc({ func: resolve }));
     });
+  };
+
+  // alert 사용시
+  const alert = ({ title, message }) => {
+    dispatch(alertOpen({ title, message }));
   };
 
   const onConfirm = () => {
@@ -22,11 +27,6 @@ const useAlert = () => {
   const onCancel = () => {
     alertInfo.resolveFunc(false);
     dispatch(closeModal());
-  };
-
-  // alert
-  const alert = ({ title, message }) => {
-    dispatch(alertOpen({ title, message }));
   };
 
   const onAlertClose = () => {
