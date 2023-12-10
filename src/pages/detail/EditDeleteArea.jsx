632abe764d56bc1fef4f154e0bfacc0ef297dce3
Kim MyungSub <audtjqxx@naver.com>
2023-12-10 22:08:22 +0900

    삭제로직 구현

--- src/pages/detail/EditDeleteArea.jsx
+++ src/pages/detail/EditDeleteArea.jsx
@@ -4,8 +4,15 @@ import { ReactComponent as Delete } from '../../styles/img/detailPage/delete.svg
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { StEditDeleteArea } from './Detail.styles';
+import { useMutation } from 'react-query';
+import { deletePost } from '../../axios/firebaseApi';
+import useAlert from 'hooks/useAlert';
 
 const EditDeleteArea = () => {
+  const { mutate: deleteMutate } = useMutation({ mutationFn: deletePost });
+
+  const { confirm, alert } = useAlert();
+
   const datas = useSelector((state) => state.currentVideoSlice.videoInfo);
 
   const navigate = useNavigate();
@@ -16,6 +23,16 @@ const EditDeleteArea = () => {
 
   const goToDeletePage = () => {
     // 삭제로직
+    deleteMutate(datas.id, {
+      onSuccess: async () => {
+        const confirmValue = await confirm({ title: '삭제', message: '해당 게시물을 삭제하시겠습니까?' });
+
+        if (!confirmValue) return;
+
+        alert({ title: '삭제완료', message: '삭제가 완료되었습니다.' });
+        navigate('/');
+      }
+    });
   };
 
   return (
